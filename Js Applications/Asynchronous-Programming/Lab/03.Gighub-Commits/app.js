42b84732df4722843d919296175f545dc2949002
Temenuzhka Gadzheva <temenujka_211@abv.bg>
2021-07-05 12:12:46 +0300

    some modifications

--- Js Applications/Asynchronous-Programming/Lab/03.Gighub-Commits/app.js
+++ Js Applications/Asynchronous-Programming/Lab/03.Gighub-Commits/app.js
@@ -1,3 +1,38 @@
 function loadCommits() {
+
+    let usernameElement = document.querySelector('#username').value;
+    let repoElement = document.querySelector('#repo').value;
+
+    let ulCommitsElement = document.querySelector('#commits');
+
+    const baseUrlElement = `https://api.github.com/repos/${usernameElement}/${repoElement}`;
     
+    ulCommitsElement.textContent = '';
+
+    let buttonElement = document.querySelector('button');
+
+    buttonElement.addEventListener('click', () => {
+
+        fetch(`${baseUrlElement}/commits`)
+            .then(res => {
+                if (res.status === 404) {
+                    throw new Error(`<li>${res.status} (${res.statusText})</li>`);
+                }
+                return res.json();
+            })
+            .then(repos => {
+
+                repos.forEach(c => {
+
+                    let liElement = document.createElement('li');
+
+                    liElement.textContent = `${c.commit.author.name} : ${c.commit.message}`;
+                 
+                    ulCommitsElement.appendChild(liElement);
+                });
+            })
+            .catch(error=>{
+                ulCommitsElement.textContent = error.message;
+            })
+    });
 }
