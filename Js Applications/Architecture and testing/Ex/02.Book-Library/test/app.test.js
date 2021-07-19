65dd80bc0277c552a4a74d38fdaaf9db22ec6178
Temenuzhka Gadzheva <temenujka_211@abv.bg>
2021-07-20 00:51:24 +0300

    testing only thrird ex

--- Js Applications/Architecture and testing/Ex/02.Book-Library/test/app.test.js
+++ Js Applications/Architecture and testing/Ex/02.Book-Library/test/app.test.js
@@ -1,14 +1,11 @@
 
 const { chromium } = require('playwright-chromium');
-const { expect } = require('chai');
+const { assert } = require('chai');
 
 let browser;
 let page;
 
-let bookLibraryUrl = `http://127.0.0.1:5500/index.html`;
-
 function fakeResponse(data) {
-
     return {
         status: 200,
         headers: {
@@ -19,139 +16,253 @@ function fakeResponse(data) {
     }
 }
 
-let corespondationMessages = {
+let createdBook = {
     1: {
         author: "Alex",
-        title: "Hello",
-        
+        title: "Brief Interviews with Hideous Men by David Foster Wallace",
     },
     2: {
         author: "Boby",
-        title: "Hey, Alex! I`m fine, thank you! And how are you?"
-    },
+        title: "The Particular Sadness of Lemon Cake by Aimee Bender"
+    }
 }
 
-describe('Book library tests ', function () {
+describe('Book library tests', function () {
+    // this.timeout(5000); 
     before(async () => {
-        // за да се виждат тестове през браузъра
         browser = await chromium.launch({ headless: false, slowMo: 1000 });
-        //  browser = await chromium.launch();
+        // browser = await chromium.launch();
     });
-    // before и  after се изпълняват по един път преди и след  всички тестове 
-    after(async () => { await browser.close(); });
 
-    // beforeEach и  afterEach се изпълняват преди и след всеки тест
+    after(async () => { await browser.close(); });
     beforeEach(async () => { page = await browser.newPage(); });
     afterEach(async () => { await page.close(); });
-
-    describe('Should load correct server', () => {
-
-        it.only("Should call correct server", async () => {
-            // подавам данните
-            await page.route('**/jsonstore/collections/books', route => route.fulfill(fakeResponse(corespondationMessages)));
-            // отивам на страницата
-            await page.goto(bookLibraryUrl);
-            // изчаквам зачвката
-            const [response] = await Promise.all([
-                page.waitForResponse('**/jsonstore/collections/books'),
-                page.click('#loadBooks'),
-            ]);
-            // проверявам резултата
-            let expectedResult = await response.json();
-            expect(expectedResult).to.eql(corespondationMessages);
-
+   
+    it('Should call correct server', async () => {
+        await page.route('**/jsonstore/collections/books', route => {
+            route.fulfill(fakeResponse(createdBook))
         });
 
-        it("Should textarea correct complete", async () => {
-            // подавам данните
-            await page.route('**/jsonstore/messenger', route => route.fulfill(fakeResponse(corespondationMessages)));
-            // отивам на страницата
-            await page.goto(messengerUrl);
+        await page.goto('http://127.0.0.1:5500');
 
-            const [response] = await Promise.all([
-                page.waitForResponse('**/jsonstore/messenger'),
-                page.click('#refresh'),
-            ]);
+        const [response] = await Promise.all([
+            page.waitForResponse('**/jsonstore/collections/books'),
+            page.click('#loadBooks')
+        ]);
 
-            // държи се като queryselector
-            // резултата който се получава
-            let messagesTextArea = await page.$eval('#messages', (textArea) => textArea.value);
-            // резултата който се очаква от сървара
-            let expectedResult = Object.values(corespondationMessages).map(v => `${v.author}: ${v.content}`).join('\n');
-            expect(messagesTextArea).to.eql(expectedResult);
-
+        let actualResult = await response.json();
+        assert.deepEqual(actualResult, createdBook);
+    });
+    it('Should create new book correctlly', async () => {
+        await page.route('**/jsonstore/collections/books', route => {
+            route.fulfill(fakeResponse(createdBook));
         });
+
+        await page.goto('http://127.0.0.1:5500');
+        await page.fill('#createForm input[name="title"]', 'Alex');
+        await page.fill('#createForm input[name="author"]', 'Brief Interviews with Hideous Men by David Foster Wallace');
+
+        const [response] = await Promise.all([
+            page.waitForResponse('**/jsonstore/collections/books'),
+            page.click('#createForm button')
+        ]);
+
+        let actualResult = await response.json();
+        assert.deepEqual(actualResult, createdBook);
+
+    });
+    it('Should new  book added correctly  in the  table', async () => {
+        await page.route('**/jsonstore/collections/books', route => {
+            route.fulfill(fakeResponse(createdBook));
+        });
+        await page.goto('http://127.0.0.1:5500');
+        await page.fill('#createForm input[name="title"]', 'Alex');
+        await page.fill('#createForm input[name="author"]', 'Brief Interviews with Hideous Men by David Foster Wallace');
+
+        const [response] = await Promise.all([
+            page.waitForResponse('**/jsonstore/collections/books'),
+            page.click('#createForm button'),
+            page.click('#loadBooks')
+        ]);
+        let actualResult = await page.textContent('tbody tr td');
+        let editButton = await page.isVisible('.editBtn');
+        let deleteButton = await page.isVisible('.deleteBtn');
+
+        assert.equal(actualResult, 'Brief Interviews with Hideous Men by David Foster Wallace');
+        assert.equal(editButton, true);
+        assert.equal(deleteButton, true);
+
     });
 
-    describe('Should correct create messages and send it', () => {
+    it("Should detete work correctlly", async () => {
 
+        let booksInTable = {
+            original: { id: "ale", book: { author: 'Alex', title: 'Brief Interviews with Hideous Men by David Foster Wallace' } },
+            deleted: { author: 'Alex', title: 'Brief Interviews with Hideous Men by David Foster Wallace' },
+        };
 
-        it("Should create complete correctlly message", async () => {
+        let respBookInTable = {
+            fillTable: {
+                status: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(createdBook),
+            },
+            del: {
+                status: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(booksInTable.deleted),
+            },
+            original: {
+                status: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(createdBook.ale),
+            },
+        };
 
-            let requestData = undefined;
+        await page.goto('http://127.0.0.1:5500');
 
-            let expectedResult = {
-                author: 'Marty',
-                content: 'Heey'
+        await page.route("**/jsonstore/collections/books*", (route) => {
+            let replies = {
+                get: respBookInTable.fillTable,
+                delete: respBookInTable.del,
             };
 
-            await page.route('**/jsonstore/messenger', (route, request) => {
-                // проверявам дали заявлата е post
-                if (request.method().toUpperCase() === 'POST') {
-                    // взимам данните
-                    requestData = request.postData();
-                    route.fulfill(fakeResponse(createdMessage))
-                }
-
-            });
-
-            await page.goto(messengerUrl);
-            // взимаме автора и съобщението 
-            await page.fill("#author", expectedResult.author);
-            await page.fill("#content", expectedResult.content);
-
-            await Promise.all([
-                page.waitForResponse('**/jsonstore/messenger'),
-                page.click('#submit'),
-            ]);
-
-            let actualResult = JSON.parse(requestData);
-
-            expect(actualResult).to.eql(expectedResult);
-
+            let method = route.request().method();
+            route.fulfill(replies[method.toLowerCase()]);
         });
 
-        it("Should clear message form", async () => {
+        await Promise.all([
+            page.waitForResponse("**/jsonstore/collections/books*"),
+            page.click("#loadBooks"),
+        ]);
 
+        page.on("dialog", (dialog) => {
+            dialog.accept();
+        });
 
-            let requestData = undefined;
-            let expectedResult = {
-                author: 'Marty',
-                content: 'Heey'
+        await page.route("**/jsonstore/collections/books/ale", (route) => {
+            assert.equal(route.request().method(), "DELETE");
+            route.fulfill(respBookInTable.del);
+        });
+
+        await page.click('tr[data-id="ale"] button.deleteBtn');
+    });
+
+    let createdBook = {
+
+        ale: {
+            author: "Alex",
+            title: "Brief Interviews with Hideous Men by David Foster Wallace",
+        },
+        bob: {
+            author: "Boby",
+            title: "The Particular Sadness of Lemon Cake by Aimee Bender"
+        }
+    }
+
+    it(" Should be correctlly data when edit book", async () => {
+
+        let originAndEditedBook = {
+            original: { id: "ale", book: { author: 'Alex', title: 'Brief Interviews with Hideous Men by David Foster Wallace' } },
+            edited: { author: 'Alex-ale', title: 'Brief Interviews with Hideous Men by David Foster Wallace-ale' },
+        };
+
+        let respBooks = {
+            fillTable: {
+                status: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(createdBook),
+            },
+            edit: {
+                status: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(originAndEditedBook.edited),
+            },
+            original: {
+                status: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(createdBook.ale),
+            },
+        };
+        await page.goto('http://127.0.0.1:5500');
+
+        await page.route("**/jsonstore/collections/books", (x) => x.fulfill(respBooks.fillTable));
+
+        await Promise.all([
+            page.waitForResponse("**/jsonstore/collections/books"),
+            page.click("#loadBooks"),
+        ]);
+
+        await page.route("**/jsonstore/collections/books/**", (route) => {
+            let replies = {
+                get: respBooks.original,
+                put: respBooks.edit,
+                delete: respBooks.del,
             };
-            await page.route('**/jsonstore/messenger', (route, request) => {
 
-                if (request.method().toUpperCase() === 'POST') {
-                    requestData = request.postData();
-                    route.fulfill(fakeResponse(createdMessage))
-                }
-            });
-
-            await page.goto(messengerUrl);
-            await page.fill("#author", expectedResult.author);
-            await page.fill("#content", expectedResult.content);
-
-            await Promise.all([
-                page.waitForResponse('**/jsonstore/messenger'),
-                page.click('#submit'),
-            ]);
-
-            let authorInput = await page.$eval('#author', a => a.value);
-            let messageInput = await page.$eval('#content', c => c.value);
-
-            expect(authorInput).to.equal('');
-            expect(messageInput).to.equal('');
-
+            let method = route.request().method();
+            route.fulfill(replies[method.toLowerCase()]);
         });
-    })
+
+        let [create, edit] = await Promise.all([
+            page.isVisible("form#createForm"),
+            page.isVisible("form#editForm"),
+        ]);
+
+        assert.equal(create, true);
+        assert.equal(edit, false);
+
+        await page.click('tr[data-id="ale"] button.editBtn');
+
+        [create, edit] = await Promise.all([
+            page.isVisible("form#createForm"),
+            page.isVisible("form#editForm"),
+        ]);
+
+        assert.equal(create, false);
+        assert.equal(edit, true);
+
+        let [bookHeader, bookWriter] = await Promise.all([
+
+            page.$eval('#editForm > input[name="title"]', (b) => b.value),
+            page.$eval('#editForm > input[name="author"]', (b) => b.value),
+        ]);
+        // дали се модифицират
+        assert.equal(bookHeader, originAndEditedBook.original.book.title);
+        assert.equal(bookWriter, originAndEditedBook.original.book.author);
+
+        await page.fill('#editForm > input[name="title"]', originAndEditedBook.edited.title);
+        await page.fill('#editForm > input[name="author"]', originAndEditedBook.edited.author);
+
+        await page.click("#editForm > button");
+
+        [bookHeader, bookWriter] = await Promise.all([
+            page.$eval('#editForm > input[name="title"]', (b) => b.value),
+            page.$eval('#editForm > input[name="author"]', (b) => b.value),
+        ]);
+        // дали се зачистват след изпращане
+        assert.equal(bookHeader, "");
+        assert.equal(bookWriter, "");
+    });
 });
+
+
+
